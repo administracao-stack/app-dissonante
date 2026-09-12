cff3a2745a3adf4ae491811886043023bd598889
administracao-stack <administracao@dissonanteexperiencias.com>
2026-09-12 15:51:47 -0300

    Update worker.py v1.0.0001

--- worker.py
+++ worker.py
@@ -1,12 +1,24 @@
 import time
-from app import app, limpar_reservas_expiradas
+from app import app, db, limpar_reservas_expiradas, FilaEmail, enviar_email_direto
+
+def processar_fila_emails():
+    emails = FilaEmail.query.limit(10).all()
+    for item in emails:
+        sucesso = enviar_email_direto(item.destinatario, item.assunto, item.corpo, item.reply_to)
+        if sucesso:
+            db.session.delete(item)
+            
+    if emails:  # Executa o commit apenas se houver registros processados
+        db.session.commit()
 
 if __name__ == '__main__':
-    print("[WORKER] Agendador ativo rodando rotinas de limpeza de reservas...")
+    print("[WORKER] Processador ativo: limpando reservas e enviando e-mails...")
     while True:
         try:
             with app.app_context():
                 limpar_reservas_expiradas()
+                processar_fila_emails()
         except Exception as e:
+            db.session.rollback()
             print(f"[ERRO WORKER]: {str(e)}")
-        time.sleep(60)  # Executa a limpeza a cada 60 segundos
+        time.sleep(10)
